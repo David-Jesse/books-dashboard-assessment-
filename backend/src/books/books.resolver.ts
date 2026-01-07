2b9750461cfcc7b76b0a403a6c77dd579c3daa08
David-Jesse <djesse351@gmail.com>
2026-01-07 05:43:58 +0100

    feat(backend) added the books-module and the resolver file

---
+++ backend/src/books/books.resolver.ts
@@ -0,0 +1,34 @@
+import {Resolver, Query, Mutation, Args, Int} from '@nestjs/graphql';
+import {UseGuards} from "@nestjs/common";
+import {Book} from './book.entity'
+import {BooksService} from "./books.service";
+import {CreateBookInput, UpdateBookInput} from "./dto/book.input";
+import {GqlAuthGuard} from "../auth/gql.auth.guard";
+
+
+@Resolver(() => Book)
+@UseGuards(GqlAuthGuard)
+export class BooksResolver {
+    constructor(private service: BooksService) {
+    }
+
+    @Query(() => [Book])
+    books() {
+        return this.service.findAll()
+    }
+
+    @Mutation(() => Book)
+    createBook(@Args('input') input: CreateBookInput) {
+        return this.service.create(input);
+    }
+
+    @Mutation(() => Book)
+    updateBook(@Args('input') input: UpdateBookInput) {
+        return this.service.update(input);
+    }
+
+    @Mutation(() => Boolean)
+    deleteBook(@Args("id", {type: () => Int}) id: number) {
+        return this.service.remove(id);
+    }
+}
