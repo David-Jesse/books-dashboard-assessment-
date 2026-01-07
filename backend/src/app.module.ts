08bfac20bd7022134a36f2b6a281bcfd4f89287a
David-Jesse <djesse351@gmail.com>
2026-01-07 04:56:42 +0100

    feat(backend): added book entity and graphql CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,40 +1,40 @@
-// import { Module } from '@nestjs/common';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { GraphQLModule } from '@nestjs/graphql';
-// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-// import { join } from 'path';
-// import { BooksModule } from './books/books.module';
-// import { AuthModule } from './auth/auth.module';
-//
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//
-//     TypeOrmModule.forRootAsync({
-//       inject: [ConfigService],
-//       useFactory: () => ({
-//         type: 'sqlite',
-//         database: 'db.sqlite',
-//         autoLoadEntities: true,
-//         synchronize: true,
-//       }),
-//     }),
-//
-//     GraphQLModule.forRootAsync<ApolloDriverConfig>({
-//       driver: ApolloDriver,
-//       inject: [ConfigService],
-//       useFactory: () => ({
-//         autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-//         sortSchema: true,
-//         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//         context: ({ req }) => ({ req }),
-//         playground: true,
-//       }),
-//     }),
-//
-//     AuthModule,
-//     BooksModule,
-//   ],
-// })
-// export class AppModule {}
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: () => ({
+        type: 'sqlite',
+        database: 'db.sqlite',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      inject: [ConfigService],
+      useFactory: () => ({
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        sortSchema: true,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        context: ({ req }) => ({ req }),
+        playground: true,
+      }),
+    }),
+
+    AuthModule,
+    BooksModule,
+  ],
+})
+export class AppModule {}
