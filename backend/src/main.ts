5067d96b5d5f52a9f11cd09ea959c1ead5469a5e
David-Jesse <djesse351@gmail.com>
2026-01-08 09:59:15 +0100

    fix(backend): enable permissive CORS for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: [
-            'http://localhost:5173',
-            'https://scrapays-assessment.netlify.app',
-        ],
+        origin: true,
         methods: ['GET', 'POST', 'OPTIONS'],
         allowedHeaders: ['Authorization', 'Content-Type'],
         credentials: true,
