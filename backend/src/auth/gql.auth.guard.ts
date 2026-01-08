1ea84ede4b0964356c52335a081c647dec979fd8
David-Jesse <djesse351@gmail.com>
2026-01-08 09:40:34 +0100

    fix(backend): enable CORS for netlify frontend

--- backend/src/auth/gql.auth.guard.ts
+++ backend/src/auth/gql.auth.guard.ts
@@ -21,6 +21,9 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
      */
     getRequest(context: ExecutionContext) {
         const ctx = GqlExecutionContext.create(context);
+        const req = ctx.getContext().req
+
+        console.log("Auth header:", req.headers.authorization)
         return ctx.getContext().req;
     }
 }
